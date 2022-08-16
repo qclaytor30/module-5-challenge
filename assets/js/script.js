2a4497f7238f15d9517bf6dea9393d8ed3b40017
qclaytor30 <qclaytor30@gmail.com>
2022-08-15 21:26:51 -0400

    create color change in a time slot based on current time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,30 @@
 // Display current date at top of page
+var currentTime = moment();
 var currentDateEl = $('#currentDate');
-var currentDate = moment().format("dddd, MMM Do");
+var currentDate = currentTime.format("dddd, MMM Do");
 currentDateEl.text(currentDate);
+
+// time-slot color
+var descriptionEls = $('.description');
+var currentHour = parseInt(currentTime.format('H'));
+
+for (var i = 0; i < descriptionEl.length; i++) {
+    var descriptionEl = descriptionEls[i];
+    var rowHour = parseInt(descriptionEl.dataset.hour);
+    //console.log(rowHour);
+    //console.log(currentHour);
+if (rowHour < currentHour) {
+    descriptionEl.addClass('past');
+}
+else if (rowHour === currentHour) {
+    descriptionEl.addClass('present');
+    //console.log("present");
+}
+else if (rowHour > currentHour) {
+    descriptionEl.addClass('future');
+    //console.log("future");
+};
+    }
+
 
 
