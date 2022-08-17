490a347e903e95c1e92651d27943eca94dbdadea
qclaytor30 <qclaytor30@gmail.com>
2022-08-17 01:56:21 -0400

    update features

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,49 @@
-// Display current date at top of page
+
 var currentTime = moment();
-var currentDateEl = $('#currentDate');
-var currentDate = currentTime.format("dddd, MMM Do");
-currentDateEl.text(currentDate);
+// Display current date at top
+var currentDayEl = $('#currentDay');
+var currentDay = currentTime.format('dddd, MMMM Do');
+currentDayEl.text(currentDay);
 
-// time-slot color
+// Set color of timeslot based on current time
 var descriptionEls = $('.description');
-var currentHour = parseInt(currentTime.format('H'));
+    // var currentHour = parseInt(currentTime.format('H'));
 
-for (var i = 0; i < descriptionEl.length; i++){
+var currentHour = 13;
+
+for (var i = 0; i < descriptionEls.length; i++) {
     var descriptionEl = descriptionEls[i];
     var rowHour = parseInt(descriptionEl.dataset.hour);
-    //console.log(rowHour);
-    //console.log(currentHour);
-if (rowHour < currentHour) {
-    descriptionEl.addClass('past');
+    if (rowHour < currentHour) {
+        descriptionEl.classList.add("past");
+    } else if (rowHour === currentHour) {
+        descriptionEl.classList.add("present");
+    } else if (rowHour > currentHour) {
+        descriptionEl.classList.add("future");
+    };
 }
-else if (rowHour === currentHour) {
-    descriptionEl.addClass('present');
-    //console.log("present");
-}
-else if (rowHour > currentHour) {
-    descriptionEl.addClass('future');
-    //console.log("future");
-};
+
+
+// add local storage save button
+function saveEvent(event) {
+    var buttonClicked = $(event.target);
+
+    var selectedTimeBlock = buttonClicked.closest('.time-block');
+    var selectedTimeblockTextarea = selectedTimeBlock.children('textarea');
+        var eventID = selectedTimeblockTextarea.attr("id");
+        var eventDescription = selectedTimeblockTextarea.val();
+
+        localStorage.setItem(eventID, JSON.stringify(eventDescription));
+
+        var messageEl = $('#message');
+        messageEl.text('Appointment added to local storage ✓');
+    }
+        var saveButtons = $('.fas');
+        saveButtons.on('click', saveEvent);
+    
+
+    // check local storage for events when page loads
+    function showSavedEvents() {
     }
 
-var saveButton = $('.saveBtn');
-
-function saveEvents() {}
-saveButton.on('click', saveEvents());
-
-function showSavedEvents(){}
-
-window.on('load', showSavedEvents());
+    window.onload = showSavedEvents(); 
