3e60e210b0c16a5576fc3456f5457e9cb821dc4c
qclaytor30 <qclaytor30@gmail.com>
2022-08-17 02:51:10 -0400

    fix cdn links, update js and local storage, and format doc

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,34 +28,34 @@ function saveEvent(event) {
 
     var selectedTimeBlock = buttonClicked.closest('.time-block');
     var selectedTimeblockTextarea = selectedTimeBlock.children('textarea');
-        var eventID = selectedTimeblockTextarea.attr("id");
-        var eventDescription = selectedTimeblockTextarea.val();
+    var eventID = selectedTimeblockTextarea.attr("id");
+    var eventDescription = selectedTimeblockTextarea.val();
 
-        localStorage.setItem(eventID, JSON.stringify(eventDescription));
+    localStorage.setItem(eventID, JSON.stringify(eventDescription));
 
-        var messageEl = $('#message');
-        messageEl.text('Task saved to local storage.');
-    }
-        var saveButtons = $('.fas');
-        saveButtons.on('click', saveEvent);
-    
-    // check local storage for events when page loads
-    function showSavedEvents() {
-        for (var i = 0; i < descriptionEls.length; i++) {
-            var descriptionEl = descriptionEls[i];
-            var descriptionID = descriptionEl.id;
-            var savedDescription = JSON.parse(localStorage.getItem(descriptionID));
-            if (savedDescription !==null){
-                descriptionEl.textContent = savedDescription;
-            }
+    var messageEl = $('#message');
+    messageEl.text('Task saved to local storage.');
+}
+var saveButtons = $('.fas');
+saveButtons.on('click', saveEvent);
+
+// check local storage for events when page loads
+function showSavedEvents() {
+    for (var i = 0; i < descriptionEls.length; i++) {
+        var descriptionEl = descriptionEls[i];
+        var descriptionID = descriptionEl.id;
+        var savedDescription = JSON.parse(localStorage.getItem(descriptionID));
+        if (savedDescription !== null) {
+            descriptionEl.textContent = savedDescription;
         }
     }
+}
 
-    window.onload = showSavedEvents(); 
+window.onload = showSavedEvents();
 // at the end of day clear local storage to start new
-    function clearLocalStorage() {
-        if (currentHour >= 18 || currentHour < 9) {
-            localStorage.clear();
-        }
+function clearLocalStorage() {
+    if (currentHour >= 18 || currentHour < 9) {
+        localStorage.clear();
     }
-    clearLocalStorage();
+}
+clearLocalStorage();
